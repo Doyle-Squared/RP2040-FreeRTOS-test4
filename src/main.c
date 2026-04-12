a97846f31e0fbc7790c137cd482ac06b9e350ed7
Doyle-Squared <adoyle28@asu.edu>
2026-04-12 14:21:15 -0700

    Finally got a working version that others can use after like four repos. Havent tested with the regular Pico yet, only the Pico W on my (Aidan's) computer

--- src/main.c
+++ src/main.c
@@ -2,10 +2,12 @@
 #include "task.h"
 #include <stdio.h>
 #include "pico/stdlib.h"
+//#include "pico/cyw43_arch.h" //PICO W ONLY
 
 
 void led_task()
-{   
+{
+    Blinker code for the regular Pico
     const uint LED_PIN = PICO_DEFAULT_LED_PIN;
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
@@ -15,6 +17,21 @@ void led_task()
         gpio_put(LED_PIN, 0);
         vTaskDelay(100);
     }
+
+
+    //CODE BELOW IS FOR THE PICO_WH, ONLY UNCOMMENT IF USING A WIRELESS PICO
+    // Initialize LED pin
+    // cyw43_arch_init();
+    
+    // // Loop forever
+    // while (true) {
+
+    //     // Blink LED
+    //     cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1); // Turn ON
+    //     sleep_ms(75);
+    //     cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0); // Turn OFF
+    //     sleep_ms(75);
+    // }
 }
 
 int main()
